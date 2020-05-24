63e701f147fa8064e7fab08b838fe3fd4d7aa570
edurekaganesh <ganesh@edureka.co>
2020-05-24 08:42:02 +0530

    first commit

---
+++ ganesh.java
@@ -0,0 +1,6 @@
+class ganesh{
+	 public static void main(String[] args){
+		 System.out.println("This is a demo program written by ganesh");
+                 System.out.println("This line added later");
+	 }
+}
